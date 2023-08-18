957f0e1ae4a571ee1e20e60430bf6013a2da4ffc
WXHRR <1147232219@qq.com>
2023-08-18 13:05:44 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
+    baseURL:'/Universal-URL/',
     head: {
       charset: 'utf-16',
       viewport: 'width=500, initial-scale=1',
